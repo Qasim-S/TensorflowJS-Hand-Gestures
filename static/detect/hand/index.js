4be1c0b563dcd4dcffc5690308e7eb52f3a81696
qasim_15 <qasim@looporigin.com>
2021-06-18 15:34:54 +0500

    added hand gesture recognition (not working)

--- static/detect/hand/index.js
+++ static/detect/hand/index.js
@@ -7,3 +7,17 @@ async function webcamLaunch() {
   const webcamIterator = await tf.data.webcam(videoElement);
 }
 webcamLaunch();
+
+(async function () {
+  // Load the MediaPipe handpose model assets.
+  const model = await handpose.load();
+
+  // Pass in a video stream to the model to obtain
+  // a prediction from the MediaPipe graph.
+  const video = document.querySelector("video");
+  const hands = await model.estimateHands(video);
+
+  // Each hand object contains a `landmarks` property,
+  // which is an array of 21 3-D landmarks.
+  hands.forEach((hand) => console.log(hand.landmarks));
+})();
