35c824caf11be3e90586465228c566649c14ce4a
qasim_15 <qasim@looporigin.com>
2021-06-20 08:42:55 +0500

    bounding box errors resolving added the codelab directory

--- static/detect/detect.js
+++ static/detect/detect.js
@@ -1,78 +1,47 @@
-var videoElement;
-async function webcamLaunch() {
-  const display = document.getElementById("display");
-  videoElement = document.createElement("video"); // Add element to display the webcam image.
-  display.appendChild(videoElement);
-  videoElement.width = 500;
-  videoElement.height = 500;
-  const webcamIterator = await tf.data.webcam(videoElement);
+const display = document.getElementById("display");
+const video = document.getElementById("webcam");
+
+// Check if webcam access is supported.
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
-webcamLaunch();
 
-async function main() {
-  // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain a
-  // hand prediction from the MediaPipe graph.
-  videoElement.addEventListener(
-    "loadeddata",
-    async function () {
-      // Video is loaded and can be played
-      // Load the MediaPipe handpose model assets.
-      const model = await handpose.load();
-      console.log("model loaded...");
-
-      // Pass in a video stream to the model to obtain
-      // a prediction from the MediaPipe graph.
-      //   const video = document.querySelector("video");
-      const hands = await model.estimateHands(videoElement);
-      console.log("hands estimated...");
-
-      // Each hand object contains a `landmarks` property,
-      // which is an array of 21 3-D landmarks.
-      hands.forEach((hand) => console.log(hand.landmarks));
-
-      //   const predictions = await model.estimateHands(
-      //     document.querySelector("video")
-      //   );
-      //   console.log("Done with predictions....");
-      //   if (predictions.length > 0) {
-      //     /*
-      //       `predictions` is an array of objects describing each detected hand, for example:
-      //       [
-      //         {
-      //           handInViewConfidence: 1, // The probability of a hand being present.
-      //           boundingBox: { // The bounding box surrounding the hand.
-      //             topLeft: [162.91, -17.42],
-      //             bottomRight: [548.56, 368.23],
-      //           },
-      //           landmarks: [ // The 3D coordinates of each hand landmark.
-      //             [472.52, 298.59, 0.00],
-      //             [412.80, 315.64, -6.18],
-      //             ...
-      //           ],
-      //           annotations: { // Semantic groupings of the `landmarks` coordinates.
-      //             thumb: [
-      //               [412.80, 315.64, -6.18]
-      //               [350.02, 298.38, -7.14],
-      //               ...
-      //             ],
-      //             ...
-      //           }
-      //         }
-      //       ]
-      //       */
-
-      //     for (let i = 0; i < predictions.length; i++) {
-      //       const keypoints = predictions[i].landmarks;
-
-      //       // Log hand keypoints.
-      //       for (let i = 0; i < keypoints.length; i++) {
-      //         const [x, y, z] = keypoints[i];
-      //         console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
-      //       }
-      //     }
-      //   }
-    },
-    false
-  );
+if (getUserMediaSupported()) {
+  enableCam();
+} else {
+  console.warn("getUserMedia() is not supported by your browser");
 }
-main();
+
+function enableCam() {
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    video: true,
+  };
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    video.srcObject = stream;
+  });
+}
+
+var children = [];
+
+randomDivGenerator = () => {
+  for (let i = 0; i < children.length; i++) {
+    display.removeChild(children[i]);
+  }
+  children.splice(0);
+
+  let left = Math.floor(Math.random() * 700);
+  let top = Math.floor(Math.random() * 700);
+  let width = Math.floor(Math.random() * 700);
+  let height = Math.floor(Math.random() * 700);
+
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "highlighter");
+  newDiv.style = `left: ${left}; top: ${top}px; width: ${width}px; height: ${height}px;`;
+
+  display.appendChild(newDiv);
+  children.push(newDiv);
+};
+
+setInterval(randomDivGenerator, 3000);
--- static/detect/hand/index.js
+++ static/detect/hand/index.js
@@ -1,6 +1,10 @@
 const enableWebcamButton = document.getElementById("webcamButton");
 const disableWebcamButton = document.getElementById("webcamButtonDisable");
 const video = document.getElementById("webcam");
+const liveView = document.getElementById("liveView");
+const display = document.getElementById("display");
+const topLeft = document.getElementById("topLeft");
+const bottomRight = document.getElementById("bottomRight");
 
 // Check if webcam access is supported.
 function getUserMediaSupported() {
@@ -50,6 +54,8 @@ handpose.load().then(function (loadedModel) {
   console.log("Got the model...");
 });
 
+var children = [];
+
 // Placeholder function for next step.
 function predictWebcam() {
   // Estimating the hands in a frame
@@ -57,7 +63,47 @@ function predictWebcam() {
   model.estimateHands(video).then(function (hands) {
     console.log("Got the hands...");
 
-    hands.forEach((hand, i) => console.log(hand.landmarks));
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+
+    hands.forEach((hand, i) => {
+      // If we are over 66% sure we are sure we detected it right, draw the bounding box!
+      if (hand.handInViewConfidence > 0.66) {
+        /*
+            tL = boundingBox.topLeft
+            bR = boundingBox.bottomRight
+            tR = [bR[0] - tL[0], tL[1]]
+            bL = [tL[0], bR[1]]
+        */
+
+        let left = Math.abs(hand.boundingBox.topLeft[0]);
+        let top = Math.abs(hand.boundingBox.topLeft[1]);
+        let width = Math.abs(
+          hand.boundingBox.bottomRight[0] - hand.boundingBox.topLeft[0]
+        );
+        let height = Math.abs(
+          hand.boundingBox.topLeft[1] - hand.boundingBox.bottomRight[1]
+        );
+
+        // Creating the highlighter div
+        const highlighter = document.createElement("div");
+        highlighter.setAttribute("class", "highlighter");
+        highlighter.style = `left: ${left}px; top: ${top}px; width: ${width}px; height: ${height}px;`;
+
+        // Appending the highlighter to the 'liveView' div
+        liveView.appendChild(highlighter);
+
+        topLeft.innerHTML = `(${hand.boundingBox.topLeft[0]}, ${hand.boundingBox.topLeft[1]})`;
+        bottomRight.innerHTML = `(${hand.boundingBox.bottomRight[0]}, ${hand.boundingBox.bottomRight[1]})`;
+
+        // Storing the highlighter in the 'children' list for deleting it safely
+        // when processing the next frame
+        children.push(highlighter);
+      }
+    });
   });
 
   // Call this function again to keep predicting when the browser is ready.
