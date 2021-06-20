cf13f5b69ff3913b645a32d042fb6352b4559ae4
qasim_15 <qasim@looporigin.com>
2021-06-20 10:20:59 +0500

    tracking hands successful

--- static/detect/hand/index.js
+++ static/detect/hand/index.js
@@ -1,110 +1,59 @@
-const enableWebcamButton = document.getElementById("webcamButton");
-const disableWebcamButton = document.getElementById("webcamButtonDisable");
 const video = document.getElementById("webcam");
-const liveView = document.getElementById("liveView");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+let trackButton = document.getElementById("webcamButton");
+let updateNote = document.getElementById("updatenote");
 
-// Check if webcam access is supported.
-function getUserMediaSupported() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
+let isVideo = false;
+let model = null;
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will
-// define in the next step.
-if (getUserMediaSupported()) {
-  enableWebcamButton.addEventListener("click", enableCam);
-  disableWebcamButton.addEventListener("click", disableCam);
-} else {
-  console.warn("getUserMedia() is not supported by your browser");
-}
-
-// Enable the live webcam view and start classification.
-function enableCam(event) {
-  // Only continue if the COCO-SSD has finished loading.
-  if (!model) {
-    return;
-  }
-
-  // getUsermedia parameters to force video but not audio.
-  const constraints = {
-    video: true,
-  };
-
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-    window.localStream = stream;
-    video.srcObject = stream;
-    video.style.visibility = "visible";
-    video.addEventListener("loadeddata", predictWebcam);
-  });
-}
-
-// Store the resulting model in the global scope of our app.
-var model = undefined;
-
-// Before we can use Handpose class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment
-// to get everything needed to run.
-// handTrack is an external object loaded from our index.html
 const modelParams = {
-  flipHorizontal: true,
-  maxNumBoxes: 20,
-  iouThreshold: 0.5,
-  scoreThreshold: 0.66,
+  flipHorizontal: true, // flip e.g for video
+  maxNumBoxes: 20, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.6, // confidence threshold for predictions.
 };
 
-handTrack.load(modelParams).then(function (loadedModel) {
-  model = loadedModel;
-  console.log("Got the model...");
-});
-
-var children = [];
-
-// Placeholder function for next step.
-function predictWebcam() {
-  // Estimating the hands in a frame
-  console.log("Getting the hands...");
-  model.detect(video).then(function (predictions) {
-    console.log(predictions);
-
-    // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
+function startVideo() {
+  handTrack.startVideo(video).then(function (status) {
+    console.log("Webcam started...", status);
+    if (status) {
+      updateNote.innerText = "Video started... Now tracking!";
+      trackButton.innerHTML = "Stop";
+      isVideo = true;
+      runDetection();
+    } else {
+      updateNote.innerText = "Please enable video!";
     }
-    children.splice(0);
-
-    predictions.forEach((prediction, i) => {
-      // If we are over 66% sure we are sure we detected it right, draw the bounding box!
-      //   if (prediction.score > 0.66) {
-      console.log("Has a hand...");
-      //  Getting the bounding box coordinates from the handTrack prediction
-      let left = prediction.bbox[0];
-      let top = prediction.bbox[1];
-      let width = prediction.bbox[2];
-      let height = prediction.bbox[3];
-
-      // Creating the highlighter div
-      const highlighter = document.createElement("div");
-      highlighter.setAttribute("class", "highlighter");
-      highlighter.style = `left: ${left}px; top: ${top}px; width: ${width}px; height: ${height}px;`;
-
-      // Appending the highlighter to the 'liveView' div
-      liveView.appendChild(highlighter);
-
-      // Storing the highlighter in the 'children' list for deleting it safely
-      // when processing the next frame
-      children.push(highlighter);
-      //   }
-    });
   });
-
-  // Call this function again to keep predicting when the browser is ready.
-  window.requestAnimationFrame(predictWebcam);
 }
 
-// Disable the webcam
-function disableCam() {
-  localStream.getVideoTracks()[0].stop();
-  video.src = "";
-  video.style.visibility = "hidden";
+function toggleVideo() {
+  if (!isVideo) {
+    trackButton.innerHTML = "Capture";
+    startVideo();
+  } else {
+    handTrack.stopVideo(video);
+    isVideo = false;
+    updateNote.innerText = "Video stopped";
+    trackButton.innerHTML = "Capture";
+  }
 }
+
+function runDetection() {
+  model.detect(video).then((predictions) => {
+    console.log("Predictions: ", predictions);
+    model.renderPredictions(predictions, canvas, context, video);
+    if (isVideo) {
+      requestAnimationFrame(runDetection);
+    }
+  });
+}
+
+// Load the model.
+handTrack.load(modelParams).then((lmodel) => {
+  // detect objects in the image.
+  model = lmodel;
+  updateNote.innerText = "Loaded Model!";
+  trackButton.disabled = false;
+});
